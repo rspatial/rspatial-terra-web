6df9ce451585bcd39ad5a2793ebd3dbde2b65cd2
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2019-08-24 13:25:06 -0700

    first

---
+++ R/run.R
@@ -0,0 +1,113 @@
+#!/usr/bin/Rscript
+
+if (tolower(Sys.info()["sysname"])=="windows"){
+	sysfun <- shell
+} else {
+	sysfun <- system		  
+}
+
+
+args <- commandArgs(TRUE)
+
+if (length(args) < 2) {
+	print(args)
+	stop("there must be at least two arguments")
+}
+
+cmd <- args[1]
+stopifnot(cmd %in% c("knit", "build"))
+
+chapters <- c("intr", "spatial", "rs")
+chapter <- tolower(args[2])
+if (chapter == "all") {
+# this is problematic as there is a lot of function hiding 
+	chapter <- chapters
+} else {
+	stopifnot(chapter %in% chapters)
+}
+
+option <- ifelse(length(args) > 2, args[3], "")
+
+oldpath <- getwd()
+
+
+
+do_build <- function(option) {
+	if (option=="pdf"){
+		x <- sysfun("make latexpdf", intern = TRUE)
+		return()
+	} else if (option=="clean"){
+		unlink("_build", recursive=TRUE)
+	} 
+	sysfun("make html")
+	ff1 <- list.files("txt", pattern="md\\.txt$", full=TRUE)
+	ff2 <- paste0("_build/html/_sources/", basename(ff1))
+	file.copy(ff1, ff2, overwrite=TRUE)
+}
+
+do_knit <- function(option) {
+
+	ff <- list.files("_R", pattern='.Rmd$', ignore.case=TRUE, full.names=TRUE)
+	kf <- list.files(".", pattern='\\.rst')
+	kf <- kf[-grep("index.rst", kf, ignore.case=TRUE)]
+	if (option=="clean"){
+		file.remove(kf)
+		file.remove(list.files("txt", full=TRUE))
+		file.remove(list.files("figures", full=TRUE))
+	} else { 
+		if (length(kf) > 0 ) {
+			stime <- file.info(ff)
+			stime <- data.frame(f=raster::extension(basename(rownames(stime)), ""), stime = stime$mtime, stringsAsFactors=FALSE)
+
+			btime <- file.info(kf)
+			btime <- data.frame(f=raster::extension(basename(rownames(btime)), ""), btime = btime$mtime, stringsAsFactors=FALSE)
+
+			m <- merge(stime, btime, by=1, all.x=TRUE)
+			m[is.na(m$btime), 'btime'] <- as.POSIXct(as.Date('2000-01-01'))
+
+			i <- which ( m$btime < m$stime ) 
+			ff <- ff[i]
+		}
+	}
+	if (length(ff) > 0) {
+		library(knitr)
+		dir.create('figures/', showWarnings=FALSE)
+		dir.create('txt/', showWarnings=FALSE)
+		md <-  raster::extension(basename(ff), '.md')
+		rst <- raster::extension(basename(ff), '.rst')
+		rcd <- file.path("txt", paste0(basename(md), ".txt"))
+		
+		opts_chunk$set(
+			dev        = 'png',
+			fig.path   = 'figures/',
+			fig.width  = 6,	fig.height = 6,
+			fig.cap="",
+			collapse   = TRUE
+		)
+		
+		for (i in 1:length(ff)) {
+			cat(paste("   ", raster::extension(basename(ff[i]), ""), "\n"))
+			knit(ff[i], md[i], envir = new.env(), encoding='UTF-8', quiet=TRUE)
+			purl(ff[i], rcd[i], quiet=TRUE)
+			pc <- paste('pandoc',  md[i], '-f markdown -t rst -o', rst[i])
+			sysfun(pc)
+			file.remove(md[i])
+		}
+	} 
+}
+
+
+
+for (ch in chapter) {
+	path <- file.path(oldpath, 'source', ch)
+	setwd(path)
+	cat(paste0("\n- ", basename(path), "\n"))
+	if (cmd == "build") {
+		do_build(option)
+	} else {
+		do_knit(option)
+	}
+}
+setwd(oldpath)
+warnings()
+
---
+++ source/index.rst
@@ -0,0 +1,34 @@
+
+Spatial Data Science with R
+============================
+
+This website provides materials to learn about spatial data analysis and modeling with *R*. *R* is a widely used programming language and software environment for data science. *R* has advanced capabilities for managing spatial data; and it provides unparalleled opportunities for analyzing such data. 
+
+
+.. topic:: 1. `Introduction to R <intr/index.html>`__  
+
+  | Start here if you have never used *R*, or if you need a refresher. (`pdf </intr/IntroductiontoR.pdf>`__)
+  | 
+  
+.. topic:: 2. `Spatial data manipulation with R <spatial/index.html>`__
+
+  | Read this to learn about the basics of reading, writing, and manipulating spatial data. (`pdf </spatial/Spatialdata.pdf>`__)
+  | 
+  
+.. topic:: 3. `Remote sensing image analysis <rs/index.html>`__
+
+  | Introduction to remote sensing (satellite) image analysis spatial data. (`pdf </rs/rs.pdf>`__)
+  |
+
+   
+
+.. This is the table of contents, must be here, but can be hidden so we can format how we like above.
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+
+   intr/index.rst
+   spatial/index.rst
+   rs/index.rst
+   
